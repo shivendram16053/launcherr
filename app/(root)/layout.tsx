634247abf33d33cff3b12da8784899922db2d0e9
shivendram16053 <shivendramishra.sm93@gmail.com>
2024-10-06 18:34:43 +0530

    Wallet conection added

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import '@/app/globals.css'
+
+
+import { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter, UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { useMemo } from 'react';
+
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(
+        () => [new PhantomWalletAdapter()],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [network]
+    );
+
+    return (
+
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    {children}
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+}
+
+
+
