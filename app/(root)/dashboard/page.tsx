fcd077caff1870452c52e489b18bc16f4c9e246c
PawanK06 <pawan06kumar2003@gmail.com>
2024-10-07 23:53:20 +0530

    add dashboard protection

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const Dashboard = () => {
+  const router = useRouter()
+  const publicKey = localStorage.getItem('publickey')
+  console.log('publicKey', publicKey)
+  if(!publicKey) {
+    router.push('/')
+  }
   return (
     <div>dashboard</div>
   )
 }
 
-export default page
+export default Dashboard
